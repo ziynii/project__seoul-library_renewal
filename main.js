f4ee4bb35e31aab78fc30d5e2d2c49f572f45d0a
ziynii <85431762+ziynii@users.noreply.github.com>
2021-07-02 21:39:13 +0900

    CSS,JS: Book list tab menu

--- main.js
+++ main.js
@@ -1,8 +1,10 @@
+'use strict';
+
 // main slide
 let currentPos = 0;
 let positionValue = 0;
 
-const IMAGE_WIDTH = 1200
+const IMAGE_WIDTH = 1200;
 
 const prevBtn = document.querySelector('.slide__prev');
 const nextBtn = document.querySelector('.slide__next');
@@ -14,15 +16,42 @@ function next() {
     images.style.transform = `translateX(${positionValue}px)`;
     currentPos += 1;
   }
-};
+}
 
 function prev() {
-	if (currentPos > 0) {
-		positionValue += IMAGE_WIDTH;
-		images.style.transform = `translateX(${positionValue}px)`;
-		currentPos -= 1;
-	}
+  if (currentPos > 0) {
+    positionValue += IMAGE_WIDTH;
+    images.style.transform = `translateX(${positionValue}px)`;
+    currentPos -= 1;
+  }
 }
 
 prevBtn.addEventListener('click', prev);
 nextBtn.addEventListener('click', next);
+
+// Book list tab menu
+let bookTargetLink = document.querySelectorAll('.main__book-button a');
+let bookTabContent = document.querySelectorAll('.main__book-content > ul');
+
+for (let i = 0; i < bookTargetLink.length; i++) {
+  bookTargetLink[i].addEventListener('click', (e) => {
+		e.preventDefault()
+    let bookOrgTarget = e.target.getAttribute('href');
+    console.log(bookOrgTarget);
+
+    let bookTabTarget = bookOrgTarget.replace('#', '');
+
+		for (let j = 0; j < bookTabContent.length; j++) {
+			bookTabContent[j].style.display = 'none'
+		}
+    document.getElementById(bookTabTarget).style.display = 'block';
+
+		for (let x = 0; x < bookTargetLink.length; x++) {
+			bookTargetLink[x].classList.remove('book__active');
+			e.target.classList.add('book__active');
+		}
+  });
+}
+document.getElementById('book-tab1').style.display = 'block';
+
+
