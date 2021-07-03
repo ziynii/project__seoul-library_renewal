3e7419f82b59edfba30d3e551387045a2b1d4f84
ziynii <85431762+ziynii@users.noreply.github.com>
2021-07-03 15:17:28 +0900

    CSS, JS: Notice and News tab menu

--- main.js
+++ main.js
@@ -35,14 +35,12 @@ let bookTabContent = document.querySelectorAll('.main__book-content > ul');
 
 for (let i = 0; i < bookTargetLink.length; i++) {
   bookTargetLink[i].addEventListener('click', (e) => {
-		e.preventDefault()
+		e.preventDefault();
     let bookOrgTarget = e.target.getAttribute('href');
-    console.log(bookOrgTarget);
-
     let bookTabTarget = bookOrgTarget.replace('#', '');
 
 		for (let j = 0; j < bookTabContent.length; j++) {
-			bookTabContent[j].style.display = 'none'
+			bookTabContent[j].style.display = 'none';
 		}
     document.getElementById(bookTabTarget).style.display = 'block';
 
@@ -55,3 +53,26 @@ for (let i = 0; i < bookTargetLink.length; i++) {
 document.getElementById('book-tab1').style.display = 'block';
 
 
+// Notice and News tab menu
+let noticeTargetLink = document.querySelectorAll('.notice__button a');
+let noticeTabContent = document.querySelectorAll('.notice__news-wrap > div');
+
+for (let a = 0; a < noticeTargetLink.length; a++) {
+	noticeTargetLink[a].addEventListener('click', (e) => {
+		e.preventDefault();
+		let noticeOrgTarget = e.target.getAttribute('href');
+		let noticeTabTarget = noticeOrgTarget.replace('#', '');
+
+		for (let b = 0; b < noticeTabContent.length; b++) {
+			noticeTabContent[b].style.display = 'none';
+		}
+		document.getElementById(noticeTabTarget).style.display = 'flex'
+
+		for (let c = 0; c < noticeTargetLink.length; c++) {
+			noticeTargetLink[c].classList.remove('notice__active');
+			e.target.classList.add('notice__active');
+		}
+	});
+}
+document.getElementById('notice-tab1').style.display = 'flex';
+document.getElementById('notice-tab2').style.display = 'none'
