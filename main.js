2b3fa3a3ff715a5adf1d4cd891dcbf3296b78242
ziynii <85431762+ziynii@users.noreply.github.com>
2021-07-04 21:45:34 +0900

    CSS, JS: Nav dropMenu

--- main.js
+++ main.js
@@ -1,5 +1,24 @@
 'use strict';
 
+
+// nav menu
+let header = document.querySelector('header');
+let mainMenuList = document.querySelectorAll('.navbar__menu > li');
+let subMenu = document.querySelectorAll('.navbar__depth-wrap');
+let headerHeight = header.offsetHeight;
+let subMenuHeight = 0;
+
+for (let i = 0; i < mainMenuList.length; i++) {
+	mainMenuList[i].addEventListener('mouseover', function() {
+		subMenuHeight = this.querySelector('ul').offsetHeight;
+		header.style.height = headerHeight + subMenuHeight + 'px';
+	});
+	mainMenuList[i].addEventListener('mouseout', function() {
+		header.style.height = headerHeight + 'px';
+	});
+}
+
+
 // main slide
 let currentPos = 0;
 let positionValue = 0;
@@ -76,3 +95,5 @@ for (let a = 0; a < noticeTargetLink.length; a++) {
 }
 document.getElementById('notice-tab1').style.display = 'flex';
 document.getElementById('notice-tab2').style.display = 'none'
+
+
